49021976ac0e036867445a9705b9d44bacb6da89
ELizabethGLiz <lizgeorge2019@gmail.com>
2024-02-05 23:11:20 -0500

    This is my first commit of fashion magazines

--- sql/fashion_magazines.sql
+++ sql/fashion_magazines.sql
@@ -1,1 +1,14 @@
--- Add your sql here
+
+select customer_name as Customer, PRINTF("$%.2f",sum(price_per_month * subscription_length)) as "Amount Due"
+from customers
+join orders, subscriptions
+on customers.customer_id = orders.customer_id
+and orders.subscription_id =  subscriptions.subscription_id 
+and subscriptions.description = 'Fashion Magazine'
+where orders.order_status='unpaid'
+Group by customers.customer_name
+having
+((select count(orders.order_status='unpaid') as unpaid_count
+from orders)
+> 1)
+
